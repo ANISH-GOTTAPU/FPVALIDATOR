079feb12566c6de7b3a5ecff886b19d6d5659c27
moharedd <mohan.reddy@keysight.com>
2026-06-24 08:54:33 +0000

    Fixed issue for deviation file

--- helpers.go
+++ helpers.go
@@ -1784,6 +1784,10 @@ func validateConfigurePoliciesSignature(root string, errs *[]string) error {
 		if info.IsDir() || !strings.HasSuffix(path, ".go") {
 			return nil
 		}
+		// Skip deviations.go files.
+		// if filepath.Base(path) == "deviations.go" {
+		// 	return nil
+		// }
 
 		fset := token.NewFileSet()
 		file, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
